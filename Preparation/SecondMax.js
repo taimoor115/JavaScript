8318a69a454fdaeaf42e7d507048657d56fdabb9
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-06-21 17:12:04 +0500

    SecondMax

--- Preparation/SecondMax.js
+++ Preparation/SecondMax.js
@@ -1,5 +1,5 @@
-// const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 
-// arr.sort((a, b) => b - a);
+arr.sort((a, b) => b - a);
 
-// console.log(arr);
+console.log(arr[1]);
